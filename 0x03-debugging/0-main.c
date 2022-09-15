4902854fba71a87afeceb69fb684d0328cee411b
Chiamaka Gold Obunma <goldthescholar@gmail.com>
2022-09-15 16:53:49 -0700

    debugging 01

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "main.h" 
+#include "main.h"
 
 /**
- * *main -test function for positive or negative
- * *return: 0
- * */ 
+ * main - test function that prints if number is positive or negative
+ * return: 0
+ */
 
-int main(void) 
+int main(void)
+{
+int i;
 
-	int i 
+i = 0;
+positive_or_negative(i);
 
-	i = 0
-	positive_or_negative(i); 
-
-	return (0);
-
+return(0);
+}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,6 @@
-#ifndef MAIN.H
-#define MAIN.H 
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h> 
-
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-void print_times_table(int n); 
-
+#include <stdio.h>
+void positive_or_negative(int i)
 #endif
